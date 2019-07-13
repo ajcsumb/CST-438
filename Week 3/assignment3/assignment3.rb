eb762ca97fe35a2efd99a573cf6c4f276b9da98c
Alfred De La Costa <ade-la-costa@csumb.edu>
2019-07-12 17:53:11 -0700

    display is working, guess is working, and hint is working. The only thing that is not working, is the ability to win the game.

--- Week 3/assignment3/assignment3.rb
+++ Week 3/assignment3/assignment3.rb
@@ -6,6 +6,7 @@ class Hangman
     @word = word
     @count = count
     @win = false
+    # An array to hold the letters guessed
     @lettersGuessed = Array.new()
 
   end 
@@ -47,7 +48,18 @@ class Hangman
   # return one of the letters in the word 
   # that has not been guessed 
   def hint()
-  
+    # Check the letters guessed array, versus the word and determine which one has not been shown
+    @word.each_char {
+      |ch|
+      # If the letter in the word does exist in the @lettersGuessed array, then return it as a hint
+      if @lettersGuessed.include?(ch) == false
+        # Add it to lettersGuessed array
+        @lettersGuessed.push(ch)
+        # Subtract one from count
+        @count -= 1
+        return ch
+      end
+    }
   end 
   
   # return a string 
@@ -57,10 +69,60 @@ class Hangman
   # Example:  if the word is cookie, and o and e have been guessed
   #   return the string "_ o o _ _ e"
   def display()
+    # The string to return
     displayString = ""
-    @word.each_char { 
-      displayString << "_ "
-    }
+    # An easy variable to hold the length of the word
+    length = @word.length
+
+    # Check to see if lettersGuessed has anything
+    if @lettersGuessed.length == 0
+      # Build the empty displayString
+      for i in 0...length
+        if i == 0
+          displayString = "_"
+        else
+          displayString << " _"
+        end
+      end
+    else
+      # If there have been letters guessed by the user, check to see if they match the word and construct the string
+      for i in 0...length
+        # Check to see if the letter has been guessed
+        if @lettersGuessed.include?(@word[i])
+          # If it has and this is the first position, just add the letter
+          if i == 0
+            displayString = @word[i]
+          else
+            # If it has been guessed, add it with a space, then add the letter
+            displayString << " " << @word[i]
+          end
+        else
+          if i == 0
+            displayString = "_"
+          else
+            displayString << " _"
+          end
+        end
+      end
+    end
+    #   @word.each_char {
+    #     |ch|
+    #     if @lettersGuessed.include?(ch)
+    #       # If the letter that is word has been guessed, add it to display String
+    #       displayString << ch << " "
+    #     else
+    #       # If the letter does not exist, add a dash
+    #       displayString << "_ "
+    #     end
+    #   }
+    # end
+    
+    
+    # begin
+    # end while 
+    # @word.each_char { 
+    #   displayString << "_"
+    # }
     return displayString
   end
  
